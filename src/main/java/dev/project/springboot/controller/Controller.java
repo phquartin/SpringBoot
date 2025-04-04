50ea777e9c8298e3d86d2e37e49c6d3b691cf0e0
phquartin <phquartinmatos@sempreceub.com>
2025-04-04 20:39:39 -0300

    feat: Add basic controller with "Ola Mundo" endpoint
    Introduced a new Spring Boot controller with a single GET endpoint at "/olamundo". This endpoint returns a simple "Ola Mundo!" message as a response.

---
+++ src/main/java/dev/project/springboot/controller/Controller.java
@@ -0,0 +1,16 @@
+package dev.project.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/olamundo")
+    public String olaMundo(){
+        return "Ola Mundo!";
+    }
+
+}
