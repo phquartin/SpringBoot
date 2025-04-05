f391dd1c06b49aacbe9f800859ed9979a6e275bd
phquartin <phquartinmatos@sempreceub.com>
2025-04-05 02:18:29 -0300

    feat: adicionar NinjaRepository para manipulação de dados
    Criação de um repositório para a entidade NinjaModel, utilizando JpaRepository para operações CRUD e integração com o Spring Data JPA. Essa implementação facilita o acesso e a persistência dos dados relacionados aos ninjas.

---
+++ src/main/java/dev/project/springboot/repository/NinjaRepository.java
@@ -0,0 +1,10 @@
+package dev.project.springboot.repository;
+
+import dev.project.springboot.model.NinjaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
+
+}
