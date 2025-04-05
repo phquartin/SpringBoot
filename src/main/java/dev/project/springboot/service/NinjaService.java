ae75acb3eee5432e8289b47e31c1682351f65598
phquartin <phquartinmatos@sempreceub.com>
2025-04-05 02:27:16 -0300

    feat: adicionar serviço para salvar Ninja
    Implementado NinjaService com método para salvar objetos Ninja. Inclui validação para garantir que a idade seja maior que zero antes de persistir no repositório.

---
+++ src/main/java/dev/project/springboot/service/NinjaService.java
@@ -0,0 +1,25 @@
+package dev.project.springboot.service;
+
+import dev.project.springboot.model.NinjaModel;
+import dev.project.springboot.repository.NinjaRepository;
+
+public class NinjaService {
+
+    private final NinjaRepository ninjaRepository;
+
+    public NinjaService(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+
+    public void saveNinja(String name, String email, int age) {
+
+        if(age <= 0){
+            throw new IllegalArgumentException("Age must be greater than zero");
+        }
+        NinjaModel ninja = new NinjaModel(name, email, age);
+
+        ninjaRepository.save(ninja);
+    }
+
+}
