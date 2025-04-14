291718cbb3e56b531d4ad11855e22034970c87f1
phquartin <phquartinmatos@sempreceub.com>
2025-04-14 12:53:41 -0300

    fix(ninja): Tranforma a Coluna de email como UNIQUE

--- src/main/java/dev/project/springboot/ninjas/model/NinjaModel.java
+++ src/main/java/dev/project/springboot/ninjas/model/NinjaModel.java
@@ -22,7 +22,7 @@ public class NinjaModel {
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false)
