a0e616340283f824dc39bfde0543d8545b594ed8
phquartin <phquartinmatos@sempreceub.com>
2025-04-15 12:42:56 -0300

    feat(ninja): Adicionado Endpoints no Controller

--- src/main/java/dev/project/springboot/ninjas/controller/NinjaController.java
+++ src/main/java/dev/project/springboot/ninjas/controller/NinjaController.java
@@ -7,23 +7,41 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping("/ninja")
 public class NinjaController {
 
     @Autowired
     private NinjaService ninjaService;
 
-    @GetMapping("/cadastro")
-    @ResponseBody
-    public String exibirCadastro(Model model) {
-        model.addAttribute("ninja", new NinjaModel());
-        return "cadastro";
+    // Adicionar Ninja (CREATE)
+    @PostMapping("/create")
+    public String createNinja() {
+        return "Ninja Criada com Sucesso!";
     }
 
-    @PostMapping("/cadastro")
-    @ResponseBody
-    public String salvarNinja(@ModelAttribute NinjaModel ninja,  Model model) {
-        ninjaService.saveNinja(ninja);
-        return "redirect:/";
+    // Procurar Ninja por ID (READ)
+    @GetMapping("/id")
+    public String showNinjasWithId() {
+        return "Ninja id!";
     }
+
+    // Mostrar todos os Ninjas (READ)
+    @GetMapping("/all")
+    public String showNinjas() {
+        return "Ninja Lista!";
+    }
+
+    // Alterar dados do Ninja (UPDATE)
+    @PutMapping("/update")
+    public String updateNinjaWithId() {
+        return "Ninja Atualizado!";
+    }
+
+    // Deletar Ninja (DELETE)
+    @DeleteMapping("/delete")
+    public String deleteNinjaWithId() {
+        return "Ninja Deletado!";
+    }
+
 }
