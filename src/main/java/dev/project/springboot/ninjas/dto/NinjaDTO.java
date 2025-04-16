0803a0b8650576ea73712dc898ec235399ff8e0e
phquartin <phquartinmatos@sempreceub.com>
2025-04-16 19:49:16 -0300

    chore(ninja): Inclui DTO e Mapper
    - Mapeia manualmente no NinjaMapper os caminhos entidade X dto

--- src/main/java/dev/project/springboot/ninjas/dto/NinjaDTO.java
+++ src/main/java/dev/project/springboot/ninjas/dto/NinjaDTO.java
@@ -1,4 +1,20 @@
 package dev.project.springboot.ninjas.dto;
 
+import dev.project.springboot.missions.model.MissionModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+    private int age;
+    private String skills;
+    private MissionModel mission;
+
 }
--- src/main/java/dev/project/springboot/ninjas/mapper/NinjaMapper.java
+++ src/main/java/dev/project/springboot/ninjas/mapper/NinjaMapper.java
@@ -1,4 +1,32 @@
 package dev.project.springboot.ninjas.mapper;
 
+import dev.project.springboot.ninjas.dto.NinjaDTO;
+import dev.project.springboot.ninjas.model.NinjaModel;
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDTO){
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setName(ninjaDTO.getName());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setAge(ninjaDTO.getAge());
+        ninjaModel.setSkills(ninjaDTO.getSkills());
+        ninjaModel.setMission(ninjaDTO.getMission());
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel){
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setName(ninjaModel.getName());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setAge(ninjaModel.getAge());
+        ninjaDTO.setSkills(ninjaModel.getSkills());
+        ninjaDTO.setMission(ninjaModel.getMission());
+        return ninjaDTO;
+    }
+
 }
